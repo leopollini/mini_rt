52b00f92b2395feaeda5ea0edb0a4f02400c2513
Simone Del Grazia <sdel-gra@c2r1p6.42firenze.it>
2024-03-15 16:29:08 +0100

    fixed tester

--- script_test_valgrind.sh
+++ script_test_valgrind.sh
@@ -3,5 +3,5 @@ output=$(ls -l ./error | grep "rw" | awk '{print $9}')
 for file in $output; do
     # Execute ./miniRT with the current file as an argument
     	echo "\e[1;31m	Testing ./miniRT error/$file\e[0m"
-	valgrind ./miniRT "error/$file" 1> grep "LEAK SUMMARY"
+	valgrind ./miniRT "error/$file" 2> grep "LEAK SUMMARY"
 done
